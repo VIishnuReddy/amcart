0afb03c8fd89c9cbfdf8957003d8eb07293e9a59
VIishnuReddy <rvishnuvardhan028@gmail.com>
2026-02-19 22:28:12 +0530

    implemented login page with security and handled custom exceptions at servlet

---
+++ src/main/java/com/example/amcart/user/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.example.amcart.user.controller;
+
+import com.example.amcart.user.dto.LoginRequest;
+import com.example.amcart.user.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request){
+         authService.login(request);
+         return ResponseEntity.ok("Login successful");
+    }
+}
